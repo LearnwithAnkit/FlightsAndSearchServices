70e1bb9c0e6cc618661f590c371e2ce8c085b5a3
Ankit <ankit.anshu145@gmail.com>
2023-05-30 15:47:29 +0530

    Updated uniqie containts

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
 };
 
 setupAndStartServer();
